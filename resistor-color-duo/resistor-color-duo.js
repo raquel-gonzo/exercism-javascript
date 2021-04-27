d84bc612fa19adc46753f66031e5ae1932b32e0a
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2021-04-26 22:32:06 -0500

    first test passes

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -1,32 +1,35 @@
+
 //
 // This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
 // convenience to get you started writing code faster.
 //
 
-const COLORS = {
-  black: 0,
-  brown: 1,
-  red: 2,
-  orange: 3,
-  yellow: 4,
-  green: 5,
-  blue: 6,
-  violet: 7,
-  grey: 8,
-  white: 9
-}
+export function decodedValue(colorCombo) {
 
-// export const 
-const decodedValue = (color1, color2) => {
-  let varColor1 = color1.toLowerCase();
-  let varColor2 = color2.toLowerCase();
+  const COLORS = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9
+  }
+
+
+  let varColor1 = colorCombo[0].toLowerCase();
+  let varColor2 = colorCombo[1].toLowerCase();
+
 
   if (COLORS.hasOwnProperty(varColor1) && COLORS.hasOwnProperty(varColor2)) {
-    let strColorVal1 = COLORS[varColor1].toString();
-    let strColorVal2 = COLORS[varColor2].toString();
+    let answer = "";
+    let intColorVal1 = COLORS[varColor1];
+    let intColorVal2 = COLORS[varColor2];
+    answer = `${intColorVal1}${intColorVal2}`
 
-    console.log(`${strColorVal1}${strColorVal2}`);
+    return(parseInt(answer));
   }
 };
-
-decodedValue("green", "WHITE");
