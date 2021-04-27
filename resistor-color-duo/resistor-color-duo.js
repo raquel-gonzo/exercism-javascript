8bc2e5cd470c957ffbc34814d880dbc993e0f84b
Rachel Gonzalez <rachelgonzalez@admins-MacBook-Pro.local>
2021-04-26 20:39:16 -0500

    test 1 passing

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -3,7 +3,7 @@
 // convenience to get you started writing code faster.
 //
 
-function decodedValue(colorCombo) {
+export function decodedValue(colorCombo) {
 
   const COLORS = {
     black: 0,
@@ -18,22 +18,18 @@ function decodedValue(colorCombo) {
     white: 9
   }
 
-  // let varColor1 = color1.toLowerCase();
-  // let varColor2 = color2.toLowerCase();
 
-
-  var colors = colorCombo.split(" ")
-  colors.splice(1, 1);
-
-  let varColor1 = colors[0].toLowerCase();
-  let varColor2 = colors[1].toLowerCase();
+  let varColor1 = colorCombo[0].toLowerCase();
+  let varColor2 = colorCombo[1].toLowerCase();
 
 
   if (COLORS.hasOwnProperty(varColor1) && COLORS.hasOwnProperty(varColor2)) {
-    let intColorVal1 = parseInt(COLORS[varColor1]);
-    let intColorVal2 = parseInt(COLORS[varColor2]);
+    let answer = "";
+    let intColorVal1 = COLORS[varColor1];
+    let intColorVal2 = COLORS[varColor2];
+    answer = `${intColorVal1}${intColorVal2}`
 
-    return (`${intColorVal1}${intColorVal2}`);
+    return(parseInt(answer));
   }
 };
 
