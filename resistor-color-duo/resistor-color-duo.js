225cc7415f633851bb93b47f0f81f9c8231922f2
Rachel Gonzalez <rachelgonzalez@admins-MacBook-Pro.local>
2021-04-26 20:22:49 -0500

    moved colors object

--- resistor-color-duo/resistor-color-duo.js
+++ resistor-color-duo/resistor-color-duo.js
@@ -3,31 +3,31 @@
 // convenience to get you started writing code faster.
 //
 
-const COLORS = {
-  black: 0,
-  brown: 1,
-  red: 2,
-  orange: 3,
-  yellow: 4,
-  green: 5,
-  blue: 6,
-  violet: 7,
-  grey: 8,
-  white: 9
-}
+function decodedValue(colorCombo) {
 
-function decodedValue([color1, color2]) {
-  let varColor1 = color1.toLowerCase();
-  let varColor2 = color2.toLowerCase();
+  const COLORS = {
+    black: 0,
+    brown: 1,
+    red: 2,
+    orange: 3,
+    yellow: 4,
+    green: 5,
+    blue: 6,
+    violet: 7,
+    grey: 8,
+    white: 9
+  }
+
+  // let varColor1 = color1.toLowerCase();
+  // let varColor2 = color2.toLowerCase();
 
 
-  // var colors = colorCombo.split(" ")
-  // colors.splice(1, 1);
+  var colors = colorCombo.split(" ")
+  colors.splice(1, 1);
 
-  // let varColor1 = colors[0].toLowerCase();
-  // let varColor2 = colors[1].toLowerCase();
+  let varColor1 = colors[0].toLowerCase();
+  let varColor2 = colors[1].toLowerCase();
 
-  // console.log(varColor1, varColor2);
 
   if (COLORS.hasOwnProperty(varColor1) && COLORS.hasOwnProperty(varColor2)) {
     let intColorVal1 = parseInt(COLORS[varColor1]);
